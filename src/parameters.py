a74d81f1cf62a512a56d53d22aead4c19216aa53
Ani <ardharma@uwaterloo.ca>
2025-06-08 22:10:25 -0400

    Add Github Workflow

---
+++ src/parameters.py
@@ -0,0 +1,10 @@
+
+
+PROJECT_ID="capable-arbor-293714"
+STAGING_DATASET = "stock_data"
+LOCATION = "US"
+URL = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+
+dataset_id = 'stock_data'
+table_id = 'stock_data_updated_via_dag'
+period = "5y"
---
+++ src/update_stock_data.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from google.cloud import bigquery
+
+from data_ingestion import *
+from parameters import dataset_id, table_id, period
+
+# Fetch base data from the URL
+df_sp500 = get_base_data('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+
+# Calculate metrics
+for tickers in df_sp500['Ticker'].unique().tolist():
+    df_sp500 = calculate_annualized_returns(ticker_symbol=tickers, df_input=df_sp500, period=period)
+
+df_sp500 = df_sp500.sort_values('Market_Cap', ascending=False).reset_index(drop=True)
+
+# Add a new column with the current date
+df_sp500['Update_Date'] = datetime.now().strftime('%Y-%m-%d')
+
+# Save the DataFrame to BigQuery
+client = bigquery.Client()
+save_table_to_bigquery(df_sp500, dataset_id=dataset_id, table_id=table_id)
