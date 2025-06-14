b71389d9f90b89059a336c60daa7d2b4b3f0101b
Ani <ardharma@uwaterloo.ca>
2025-06-14 15:14:49 -0400

    Added flow name and schedule

--- scripts/update_stock_data.py
+++ scripts/update_stock_data.py
@@ -1,8 +1,6 @@
-# deploy_stock_update.py
 from prefect import flow, task
 
 from datetime import datetime
-from google.cloud import bigquery
 
 from functions.data_ingestion import *
 from parameters import dataset_id, table_id, period
@@ -11,7 +9,7 @@ from parameters import dataset_id, table_id, period
 def update_stock_data():
     # Fetch base data from the URL
     df_sp500 = get_base_data('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    df_sp500 = df_sp500.head(20)
+    df_sp500 = df_sp500.head()
 
     # Calculate metrics
     for tickers in df_sp500['Ticker'].unique().tolist():
@@ -24,12 +22,11 @@ def update_stock_data():
     df_sp500['Update_Date'] = datetime.now().strftime('%Y-%m-%d')
 
     # Save the DataFrame to BigQuery
-    client = bigquery.Client()
     save_table_to_bigquery(df_sp500, dataset_id=dataset_id, table_id=table_id)
 
     return f"Successfully Update Stock Data in BigQuery {datetime.now().strftime('%Y-%m-%d')}"
 
-@flow(log_prints=True)
+@flow(log_prints=True, name="update-bigquery")
 def main():
     update_stock_data()
 
@@ -38,4 +35,5 @@ if __name__ == "__main__":
         name="update-stock-data-bigquery",
         work_pool_name="default-work-pool",
         image="anizehrs/ani-docker:latest",
+        interval=21600
     )
