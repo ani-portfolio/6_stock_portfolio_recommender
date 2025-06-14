a62d133a7fd4682f8f52c0f2a09900eabe356b67
Ani <ardharma@uwaterloo.ca>
2025-06-14 18:54:33 -0400

    Added branch

--- scripts/update_stock_data.py
+++ scripts/update_stock_data.py
@@ -1,4 +1,5 @@
 from prefect import flow, task
+
 from datetime import datetime
 
 from functions.data_ingestion import *
@@ -11,8 +12,8 @@ def update_stock_data():
     df_sp500 = df_sp500.head()
 
     # Calculate metrics
-    for ticker in df_sp500['Ticker'].unique().tolist():
-        df_sp500 = calculate_annualized_returns(ticker_symbol=ticker, df_input=df_sp500, period=period)
+    for tickers in df_sp500['Ticker'].unique().tolist():
+        df_sp500 = calculate_annualized_returns(ticker_symbol=tickers, df_input=df_sp500, period=period)
     print(f"Calculated annualized returns for {len(df_sp500['Ticker'].unique().tolist())} tickers.")
 
     df_sp500 = df_sp500.sort_values('Market_Cap', ascending=False).reset_index(drop=True)
@@ -30,17 +31,11 @@ def main():
     update_stock_data()
 
 if __name__ == "__main__":
-    # Deploy the flow using the new method
     main.from_source(
-        source="https://github.com/ani-portfolio/6_stock_portfolio_recommender.git",
-        entrypoint="update_stock_data.py:main",
+        source="https://github.com/ani-portfolio/6_stock_portfolio_recommender.git#feature/prefect",
+        entrypoint="scripts/update_stock_data.py:main",
     ).deploy(
         name="update-stock-data-bigquery",
         work_pool_name="default-work-pool",
-        job_variables={"env": {"PREFECT_LOGGING_LEVEL": "INFO"}},
-        cron="0 */6 * * *",  # Run every 6 hours
-        tags=["stock-data", "bigquery"],
-        description="Updates stock data in BigQuery every 6 hours",
-        version="1.0.0",
-        path="scripts",  # Specify the path within the repo
+        interval=21600
     )
